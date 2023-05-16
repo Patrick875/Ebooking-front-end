e67cd2514d29822154e815ae4cd620a258ad1aaa
KUBWIMANA Patrick <52809972+Patrick875@users.noreply.github.com>
2023-05-16 16:23:07 +0200

    added users on purchase orders,stock orders and sells pending

--- e-booking/src/views/pages/stock/StockRequestView.js
+++ e-booking/src/views/pages/stock/StockRequestView.js
@@ -1,118 +1,76 @@
-import React, { useState, useMemo, useRef } from 'react'
-import { CCard, CCardBody, CCardHeader } from '@coreui/react'
+import {
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CImage,
+  CRow,
+  CTable,
+  CTableBody,
+  CTableDataCell,
+  CTableHead,
+  CTableHeaderCell,
+  CTableRow,
+} from '@coreui/react'
+import React, { useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
-import { AgGridReact } from 'ag-grid-react'
 import ReactToPrint from 'react-to-print'
 import PrintHeader from '../Printing/PrintHeader'
 import PrintFooterNoSignatures from '../Printing/PrintFooterNoSignature'
 import { instance } from 'src/API/AxiosInstance'
 import { toast } from 'react-hot-toast'
 import BackButton from 'src/components/Navigating/BackButton'
-import 'ag-grid-community/styles/ag-grid.css'
-import 'ag-grid-community/styles/ag-theme-alpine.css'
 
 const Request = (props, ref) => {
-  const containerStyle = { width: '100%', height: '100%' }
-  const gridStyle = { height: 500, width: '100%' }
-  const request = useSelector((state) => state.selection.selected)
-  let stockOrderDetails
-  if (request && request.PetitStockRequesitionDetails) {
-    stockOrderDetails = request.PetitStockRequesitionDetails
-
-    stockOrderDetails =
-      stockOrderDetails.length !== 0
-        ? stockOrderDetails.map((el) => {
-            return {
-              ...el,
-              total: Number(el.quantity * el.StockItemValue.price),
-            }
-          })
-        : stockOrderDetails
-  }
-  const totalRequestGetter = () => {
-    if (stockOrderDetails && stockOrderDetails.length !== 0) {
-      return stockOrderDetails.reduce(
-        (acc, el) => acc + Number(el.quantity * el.StockItemValue.price),
-        0,
-      )
-    }
-    return 0
-  }
-  const totalGetter = (params) => {
-    const stockItemValue = params.data?.StockItemValue
-    const quantity = params.data?.quantity
-
-    if (stockItemValue && stockItemValue.price && quantity) {
-      return Number(stockItemValue.price * quantity).toLocaleString()
-    } else {
-      return totalRequestGetter().toLocaleString()
-    }
-  }
-  const totalRow = {
-    StockItemValue: { StockItemNew: { name: 'Total' } },
-    quantity: '',
-    'StockItemValue.price': '',
-    totalPrice: totalRequestGetter,
-  }
-  const [rowData, setRowData] = useState([...stockOrderDetails, totalRow])
-
-  const columnDefs = useMemo(
-    () => [
-      {
-        field: 'StockItemValue.StockItemNew.name',
-        headerName: 'Designation',
-        editable: false,
-      },
-      {
-        field: 'quantity',
-        headerName: 'Qty',
-        editable: true,
-      },
-      { field: 'StockItemValue.price', headerName: 'P.U', editable: true },
-      {
-        headerName: 'T.P',
-        field: 'totalPrice',
-        valueGetter: totalGetter,
-      },
-    ],
-    [],
-  )
-  const gridApiRef = useRef(null)
-
-  const onGridReady = (params) => {
-    params.api.sizeColumnsToFit()
-    gridApiRef.current = params.api // Save reference to the grid API
-  }
-
-  const defaultColDef = useMemo(() => {
-    return {
-      editable: true,
-    }
-  }, [])
-
+  const { request, stockOrderDetails } = props
+  const orderTotal = request && request.total ? request.total : 0
   return (
-    <div className="card">
-      <div className="m-3 p-3">
-        <h2 className="text-center my-3">
-          Stock order N &#176; {request ? request.stockRequesitionId : null}
-        </h2>
-      </div>
-      <div style={containerStyle}>
-        <div style={gridStyle} className="ag-theme-alpine">
-          <AgGridReact
-            rowData={rowData}
-            onGridReady={onGridReady}
-            onFirstDataRendered={() => {
-              if (gridApiRef.current) {
-                gridApiRef.current.sizeColumnsToFit()
-              }
-            }}
-            animateRows={true}
-            columnDefs={columnDefs}
-            defaultColDef={defaultColDef}
-          ></AgGridReact>
+    <div className="m-3 p-3">
+      <h2 className="text-center my-3">
+        Stock order N &#176; {request ? request.stockRequesitionId : null}
+      </h2>
+
+      <CCardBody className="d-flex justify-content-around">
+        <div className="col">
+          <CTable bordered>
+            <CTableHead>
+              <CTableRow>
+                <CTableHeaderCell scope="col">Designation</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Qty</CTableHeaderCell>
+                <CTableHeaderCell scope="col">P.U</CTableHeaderCell>
+                <CTableHeaderCell scope="col">T.P</CTableHeaderCell>
+              </CTableRow>
+            </CTableHead>
+
+            <CTableBody>
+              {stockOrderDetails && stockOrderDetails.length !== 0
+                ? stockOrderDetails.map((order, i) => (
+                    <CTableRow key={i}>
+                      <CTableDataCell>
+                        {order.StockItemValue.StockItemNew.name}
+                      </CTableDataCell>
+                      <CTableDataCell>{order.quantity}</CTableDataCell>
+                      <CTableDataCell>
+                        {order.StockItemValue.price}
+                      </CTableDataCell>
+                      <CTableDataCell>
+                        {Number(
+                          Number(order.quantity) *
+                            Number(order.StockItemValue.price),
+                        ).toLocaleString()}
+                      </CTableDataCell>
+                    </CTableRow>
+                  ))
+                : null}
+              <CTableRow>
+                <CTableHeaderCell colSpan={3}>Total</CTableHeaderCell>
+                <CTableHeaderCell>
+                  {Number(orderTotal).toLocaleString()}
+                </CTableHeaderCell>
+              </CTableRow>
+            </CTableBody>
+          </CTable>
         </div>
-      </div>
+      </CCardBody>
     </div>
   )
 }
@@ -126,7 +84,7 @@ const StockRequestView = React.forwardRef((props, ref) => {
     stockOrderDetails = request.PetitStockRequesitionDetails
   }
 
-  const approveStockOrder = async (quantity) => {
+  const approveStockOrder = async () => {
     await instance
       .post('petitstock/order/approve', { request: request.id })
       .then(() => {
@@ -170,11 +128,9 @@ const StockRequestView = React.forwardRef((props, ref) => {
       </CCardHeader>
 
       <div style={{ display: 'none' }}>
-        <div ref={ref ? ref.current || componentRef : componentRef}>
+        <div ref={ref || componentRef}>
           <PrintHeader />
-          <div className="mx-2">
-            <Request request={request} stockOrderDetails={stockOrderDetails} />
-          </div>
+          <Request request={request} stockOrderDetails={stockOrderDetails} />
           <PrintFooterNoSignatures />
         </div>
       </div>
